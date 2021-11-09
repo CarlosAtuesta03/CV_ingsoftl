52d77890f2d6846c07e00cc5ee062c17d0c6fa3a
Carlos Atuesta 03 <Carlos Atuesta 03@LAPTOP-OU0QS98O>
2021-11-09 18:04:21 -0500

    Version 1

--- CV_ejercicio/src/cv_ejercicio/CV_ejercicio.java
+++ CV_ejercicio/src/cv_ejercicio/CV_ejercicio.java
@@ -16,6 +16,7 @@ public class CV_ejercicio {
      */
     public static void main(String[] args) {
         System.out.println("Versión 0");
+        System.out.println("Versión 1");
     }
     
 }
